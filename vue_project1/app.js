fad8e983f02ed4695534ee0a179af5c14e5d28d4
Denis K <wolf-den@tut.by>
2023-06-04 15:41:26 +0300

    feat: add vue_project1

---
+++ vue_project1/app.js
@@ -0,0 +1,39 @@
+const App = {
+  data() {
+    return {
+      myPlaceholder: 'Введите название заметки',
+      title: 'Список заметок',
+      inputValue: '',
+      notes: ['zametka 1', 'заметка 2']
+    }
+  },
+  methods: {
+    addNewNote() {
+      if (this.inputValue) {
+        this.notes.push(this.inputValue);
+        this.inputValue = '';
+      }
+    },
+    toUpperCase(item) {
+      return item.toUpperCase();
+    },
+    removeNote(idx) {
+      this.notes.splice(idx, 1);
+    }
+  },
+  computed: {
+    doubleCountComputed() {
+      console.log('doubleCountComputed')
+      return this.notes.length * 2;
+    },
+  },
+  watch: {
+    inputValue(value) {
+      if (value.length > 10) {
+        this.inputValue = ''
+      }
+    }
+  }
+}
+
+const app = Vue.createApp(App).mount('#app');
